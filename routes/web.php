dd96dcdf292dfd4f3a0b9c97658d8d9e49104865
Ritu <ritu.gubhaju20@gmail.com>
2022-07-25 11:32:40 +0545

    first push

---
+++ routes/web.php
@@ -0,0 +1,98 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return redirect(route('login'));
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+// Route::get('/birthday', [App\Http\Controllers\HomeController::class, 'birthdayNotification'])->name('birthday');
+
+Route::group(['as' => 'user.','namespace' => 'App\Http\Controllers', 'prefix' => 'user',], function () {
+    Route::get('forget-password', 'User\UserController@forgetPassword')->name('forgetPassword');
+    Route::post('update-password', 'User\UserController@updatePassword')->name('updatePassword');
+
+});
+
+Route::group(['middleware' => 'auth','namespace' => 'App\Http\Controllers'], function () {
+    Route::get('/dashboard','Dashboard\DashboardController@index')->name('dashboard');
+
+
+
+
+    /*
+    |--------------------------------------------------------------------------
+    | User CRUD
+    |--------------------------------------------------------------------------
+    |
+    */
+
+    Route::group(['as' => 'user.', 'prefix' => 'user',], function () {
+        Route::get('', 'User\UserController@index')->name('index')->middleware('permission:user-index');
+        Route::get('user-data', 'User\UserController@getAllData')->name('data')->middleware('permission:user-data');
+        Route::get('create', 'User\UserController@create')->name('create')->middleware('permission:user-create');
+        Route::post('', 'User\UserController@store')->name('store')->middleware('permission:user-store');
+        Route::get('{user}/edit', 'User\UserController@edit')->name('edit')->middleware('permission:user-edit');
+        Route::put('{user}', 'User\UserController@update')->name('update')->middleware('permission:user-update');
+        Route::get('user/{id}/destroy', 'User\UserController@destroy')->name('destroy')->middleware('permission:user-delete');
+        Route::get('update-profile', 'User\UserController@profileUpdate')->name('profileUpdate');
+        Route::post('update-profile/{id}', 'User\UserController@profileUpdateStore')->name('updateProfile');
+
+    });
+
+    /*
+    |--------------------------------------------------------------------------
+    | Role CRUD
+    |--------------------------------------------------------------------------
+    |
+    */
+
+    Route::group(['as' => 'role.', 'prefix' => 'role',], function () {
+        Route::get('', 'Role\RoleController@index')->name('index')->middleware('permission:role-index');
+        Route::get('role-data', 'Role\RoleController@getAllData')->name('data')->middleware('permission:role-data');
+        Route::get('create', 'Role\RoleController@create')->name('create')->middleware('permission:role-create');
+        Route::post('', 'Role\RoleController@store')->name('store')->middleware('permission:role-store');
+        Route::get('{role}/edit', 'Role\RoleController@edit')->name('edit')->middleware('permission:role-edit');
+        Route::put('{role}', 'Role\RoleController@update')->name('update')->middleware('permission:role-update');
+        Route::get('role/{id}/destroy', 'Role\RoleController@destroy')->name('destroy')->middleware('permission:role-delete');
+    });
+
+    /*
+    |--------------------------------------------------------------------------
+    | Permission CRUD
+    |--------------------------------------------------------------------------
+    |
+    */
+
+    Route::group(['as' => 'permission.', 'prefix' => 'permission',], function () {
+        Route::get('', 'Permission\PermissionController@index')->name('index')->middleware('permission:role-index');
+        Route::get('permission-data', 'Permission\PermissionController@getAllData')->name('data')->middleware('permission:role-data');
+        Route::get('create', 'Permission\PermissionController@create')->name('create')->middleware('permission:permission-create');
+        Route::post('', 'Permission\PermissionController@store')->name('store')->middleware('permission:role-store');
+        Route::get('{permission}/edit', 'Permission\PermissionController@edit')->name('edit')->middleware('permission:permission-edit');
+        Route::put('{permission}', 'Permission\PermissionController@update')->name('update')->middleware('permission:role-update');
+        Route::get('permission/{id}/destroy', 'Permission\PermissionController@destroy')->name('destroy')->middleware('permission:permission-delete');
+    });
+
+
+    Route::group(['as'=>'common.', 'prefix'=>'common'], function(){
+        Route::post('provinces', 'Common\CommonController@getProvincesByCountryId')->name('province.countryId');
+        Route::post('districts', 'Common\CommonController@getDistrictsByProvinceId')->name('district.provinceId');
+    });
+
+
+});
